f79cde3ccbcec036fbe38e5a3364fba770ae56de
CRoach02 <cooperroach02@gmail.com>
2024-04-13 15:10:29 -0400

    Remove generated files Refactor RomanNumeral files

--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -1,6 +1,11 @@
+import RomanNumerals.RomanNumeral;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
+    RomanNumeral romanNumeral = new RomanNumeral();
 
+    @Test
+    public void RomanNumeralTest1(){
 
+    }
 }
